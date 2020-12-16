d3be489cedb5acd3d24efc9c2ac09b4948e5bd59
medkan01 <mehdiakniou@gmail.com>
2020-12-16 14:57:21 +0100

    client.c

--- client.c
+++ client.c
@@ -13,6 +13,7 @@
 
 
 //declaration des constantes
+#define LOCALHOST "127.0.0.1"
 #define PORT 3000 //port de connexion au serveur
 #define MAX_BUFFER 1000 //buffer
 
@@ -27,4 +28,31 @@ int main(int argc, char const *argv[])
 
     //initialisation socket
     fdSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if(fdSocket < 0){
+        printf("Erreur ! Socket incorrect");
+        exit(EXIT_FAILURE);
+    } else {
+        //preparation coordServeur
+        longueurAdresse = sizeof(struct sockaddr_in);
+        memset(&coordServeur, 0x00, longueurAdresse);
+        coordServeur.sin_family = PF_INET;
+        //adresse serveur en local
+        inet_aton(LOCALHOST, &coordServeur.sin_addr);
+        //Interfaces dispo
+        coordServeur.sin_port = (htons(PORT));
+        if(connect(fdSocket, (struct sockaddr *) &coordServeur, sizeof(coordServeur)) == -1){
+            printf("Connexion impossible\n");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Connexion réussie !\n");
+            strcpy(tampon, "Message du client vers le serveur");
+            send(fdSocket, tampon, strlen(tampon), 0);
+            nbRecu = recv(fdSocket, tampon, 99, 0);
+            if(nbRecu > 0){
+                tampon[nbRecu] = 0;
+                printf("Reçu: %s", tampon);
+            }
+            close(fdSocket);
+        }
+    }
 }
