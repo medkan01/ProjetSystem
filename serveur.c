acd3eea85460a4cb4d919ee6c5cbeb3d18f616b4
medkan01 <mehdiakniou@gmail.com>
2020-12-16 10:09:46 +0100

    structure salle

---
+++ serveur.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PORT 1000 //port de connexion
+#define MAX_BUFFER 1000 //buffer
+#define boolean int //type booleen
+#define true 1 //
+#define false 0 //
+#define MAX_PLACES 100 //nombre maximum de places
+
+typedef struct{
+    boolean instanceOf; //boolean permettant de savoir si la structure actuelle est utilisée ou non
+
+    int places_occupees; //nombre de places occupées
+    int places_libres; //nombre de places libres dans la salle
+} salle;
+
